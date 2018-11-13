1303b7eddce46eaea9f32a3da3f2ee1b722d0fe2
John Flatness <john@zerocrates.org>
2018-11-13 16:05:22 -0500

    Fix suzy path

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('css', function () {
     return gulp.src('./asset/sass/*.scss')
         .pipe(sass({
             outputStyle: 'compressed',
-            includePaths: ['../../node_modules/susy/sass']
+            includePaths: ['node_modules/susy/sass']
         }).on('error', sass.logError))
         .pipe(postcss([
             autoprefixer()
